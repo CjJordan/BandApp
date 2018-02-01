a8c316736eeb7171a23bf668bc28cd20b62a1fc9
Cj Jordan <cj.jordan.tech@gmail.com>
2018-02-01 11:15:35 -0600

    url path

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ console.log("working");
 $(document).on("click", function() {
     window.location.replace("https://accounts.spotify.com/en/authorize?client_id=84dbfb40bf444d6bb409195e34dcd32d&response_type=token&redirect_uri=https://cjjordan.github.io/BandApp/");
 })
+
+console.log("Url is", window.location.href);
 // var queryURL = "";
 
 // $.ajax({
